9d2211de0e98f81f22cd8199e8390215e57008ba
g4rder <ilyagarder@mail.ru>
2024-09-10 21:40:53 +0700

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,7 @@
+my_string = input()
+print(len(my_string)) # len() - считает кол-во символов
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(' ',''))
+print('First symbol:', my_string[0:1:])
+print('Last symbol:', my_string[-1])
